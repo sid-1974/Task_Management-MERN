f02ad2d1c26e4e1c6d746d3fff9315159430b38d
sid-1974 <sidgithub1974@gmail.com>
2024-09-16 00:00:56 +0530

    frontend added

---
+++ frontend/src/App.jsx
@@ -0,0 +1,57 @@
+import React, { useEffect, useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Login from "./components/Login";
+import Home from "./components/Home";
+import Register from "./components/Register";
+import Profile from "./components/Profile";
+import { Toaster } from "react-hot-toast";
+import axios from "axios";
+
+function App() {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [tasks, setTasks] = useState([]);
+  const [user, setUser] = useState([]);
+  const [TaskTitle,setTaskTitle] = useState("Tasks")
+
+  useEffect(() => {
+    const handleGetUser = async () => {
+      try {
+        const { data } = await axios.get(
+          "http://localhost:4000/api/v1/user/me",
+          { withCredentials: true }
+        );
+        setIsAuthenticated(true);
+        setUser(data.user);
+      } catch (error) {
+        console.log("USER IS NOT AUTHENTICATED");
+        setIsAuthenticated(false);
+        setUser({});
+      }
+    };
+    handleGetUser();
+  }, [isAuthenticated]);
+
+  return (
+    <Router>
+      <Header
+        setTasks={setTasks}
+        tasks={tasks}
+        setIsAuthenticated={setIsAuthenticated}
+        isAuthenticated={isAuthenticated}
+        setTaskTitle={setTaskTitle}
+        TaskTitle={TaskTitle}
+
+      />
+      <Routes>
+        <Route path="/" element={<Home isAuthenticated={isAuthenticated} tasks={tasks} setTasks={setTasks} />} />
+        <Route path="/login" element={<Login isAuthenticated={isAuthenticated} setIsAuthenticated={setIsAuthenticated} />} />
+        <Route path="/register" element={<Register isAuthenticated={isAuthenticated} setIsAuthenticated={setIsAuthenticated} />} />
+        <Route path="/profile" element={<Profile user={user} isAuthenticated={isAuthenticated}/>} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,110 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import {Link, useNavigate} from 'react-router-dom'
+import '../css/Header.css'
+import Button from 'react-bootstrap/esm/Button';
+import axios from 'axios'
+import toast from 'react-hot-toast'
+import { useEffect, useState } from 'react';
+
+function Header({setTasks,tasks,setIsAuthenticated,isAuthenticated,setTaskTitle}) {
+
+  const [allTasks, setAllTasks] = useState([]);
+  const navigate = useNavigate()
+  // Fetch tasks from the server when the component mounts
+  useEffect(() => {
+    fetchTasks();
+  }, [isAuthenticated]);
+
+  // Fetch tasks from the server
+  const fetchTasks = async () => {
+    try {
+      const response = await axios.get(
+        "http://localhost:4000/api/v1/task/mytask",
+        { withCredentials: true }
+      );
+      setAllTasks(response.data.tasks);
+      setTasks(response.data.tasks); // Update tasks with fetched tasks
+    } catch (error) {
+      console.error("Error fetching tasks:", error);
+    }
+  };
+
+
+
+
+
+
+  const handleLogout = async () => {
+    try {
+      const { data } = await axios.get(
+        "http://localhost:4000/api/v1/user/logout",
+        { withCredentials: "true" }
+      );
+      toast.success(data.message);
+      setIsAuthenticated(false);
+      navigate("/login")
+    } catch (error) {
+      toast.error(error.response.data.message);
+    }
+  };
+
+
+   const filterTasks = (filterType) => {
+    let filteredTasks = [];
+
+     switch (filterType) {
+       case "completed":
+        filteredTasks = allTasks.filter((task) => task.status === "completed");
+        setTaskTitle("Completed Tasks");
+         break;
+      case "incomplete":
+        filteredTasks = allTasks.filter((task) => task.status === "incomplete");
+        setTaskTitle("Incomplete Tasks");
+        break;
+       case "archived":
+         filteredTasks = allTasks.filter((task) => task.archived === true);
+         setTaskTitle("Archived Tasks");
+        break;
+       case "all":
+         filteredTasks = allTasks;
+        setTaskTitle("Tasks");
+        break;
+      default:
+         filteredTasks = allTasks;
+     }
+     setTasks(filteredTasks);
+   };
+
+
+
+
+
+  return (
+    <Navbar expand="lg" className={`bg-body-tertiary  ${!isAuthenticated ? "d-none":""}`}>
+      <Container>
+        <Navbar.Brand href="#home">Task Manager</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="navbar">
+      <Link to="/" className="link">Home</Link>
+
+      
+      <NavDropdown title="Filter Tasks" id="basic-nav-dropdown" className="nav-dropdown">
+        <NavDropdown.Item onClick={() => filterTasks("all")} className="nav-dropdown-item">All Tasks</NavDropdown.Item>
+        <NavDropdown.Item onClick={() => filterTasks("completed")} className="nav-dropdown-item">Completed Tasks</NavDropdown.Item>
+        <NavDropdown.Item onClick={() => filterTasks("incomplete")} className="nav-dropdown-item">Incomplete Tasks</NavDropdown.Item>
+        <NavDropdown.Item onClick={() => filterTasks("archived")} className="nav-dropdown-item">Archived Tasks</NavDropdown.Item>
+      </NavDropdown>
+      <Link to="/profile" className="profile">Profile</Link>
+      <Button className='logout-button' onClick={handleLogout}>Logout</Button>
+    </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default Header;
